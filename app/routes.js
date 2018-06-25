d8693d0327d519e8484bc50a8b89b97eb667e0c4
Daniel Goje <gojedan98@yahoo.com>
2018-06-25 13:28:29 +0300

    Create functionality for login and profile pages

--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,35 @@
 "use strict";
 const request = require("request-promise");
+const baseUri = "http://api.steampowered.com";
 
 module.exports = (app, passport) => {
 
-    app.get('/', (req, res) => {
-    
+    app.get('/', async(req, res) => {
         if(req.user) {
-            request(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${req.user.id.match(/\d+$/)[0]}`)
-                .then(data => JSON.parse(data))
-                .then((data) => {
-                    res.render("pages/home.ejs", {
-                        user: data.response.players[0]
-                    });
-                });
+            let currentUser = await getCurrentUser(req.user.steamId);
+            res.render("pages/home.ejs", {
+                currentUser: currentUser
+            });
         } else {
             res.render("pages/home.ejs", {
-                user: false
+                currentUser: false
+            });
+        }
+    });
+    app.get("/user/profile/:id", isLoggedIn, async(req, res) => {
+        let currentUser = await getCurrentUser(req.user.steamId);
+        if(req.params.id == req.user.steamId) {
+            res.render("pages/profile.ejs", {
+                currentUser: currentUser,
+                queriedUser: currentUser
+            });
+        } else {
+            let user = {};
+            user.personaname = "Lorem Ipsum";
+            user.avatar = "http://i0.kym-cdn.com/photos/images/original/000/002/941/Duckroll.jpg";
+            res.render("pages/profile.ejs", {
+                currentUser: currentUser,
+                queriedUser: user
             });
         }
     });
@@ -35,3 +49,17 @@ module.exports = (app, passport) => {
         res.redirect(req.get("Referer") || '/');
     })
 };
+
+let isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()) {
+      return next();
+    }
+  
+    res.redirect('/');
+};
+
+let getCurrentUser = async(userId) => {
+    let jsonData = await request(`${baseUri}/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${userId}`);
+    let data = JSON.parse(jsonData);
+    return data.response.players[0];
+}
