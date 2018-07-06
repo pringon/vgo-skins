c806ee3660c4bf14bcc86b734408a56533637b66
Daniel Goje <gojedan98@yahoo.com>
2018-07-06 21:14:48 +0300

    Implement select gambled items modal

--- assets/scripts/item_selection.js
+++ assets/scripts/item_selection.js
@@ -1,10 +1,13 @@
 "use strict";
+let currentSelectedItems = {};
+let currentMoneyGambled = {};
 let selectedItems = {};
 let totalMoneyGambled = 0;
 
 function selectItem(event) {
-    let element = $(event.target);
     
+    let element = $(event.target);
+
     if(element.hasClass("selected-item")) {
     
         element.removeClass("selected-item");
@@ -22,4 +25,30 @@ function selectItem(event) {
 
         $("#total-gambled").text(`Total: ${totalMoneyGambled.toFixed(2)}$`);
     }
+
+    if(Object.keys(selectedItems).length !== 0 && selectedItems.constructor === Object) {
+
+        $(".modal-footer > button").addClass("btn-info");
+        $(".modal-footer > button").removeClass("btn-basic");
+        $(".modal-footer > button").prop("disabled", false);
+    } else {
+
+        $(".modal-footer > button").addClass("btn-basic");
+        $(".modal-footer > button").removeClass("btn-info");
+    }
+}
+
+function submitSelection() {
+
+    console.log(`stake is ${totalMoneyGambled}`);
+    socket.emit("items gambled", totalMoneyGambled.toFixed(2));
+    currentSelectedItems = selectedItems;
+    currentMoneyGambled = totalMoneyGambled;
+}
+
+function clearSelection() {
+
+    selectedItems = {};
+    totalMoneyGambled = 0;
+    $("#total-gambled").text("Total: 0.00$");
 }
