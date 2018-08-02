3707558f9cd0f4adb965775a341ab7cec3d72ed6
Daniel Goje <gojedan98@yahoo.com>
2018-08-02 14:27:54 +0300

    Jackpot alpha version 1.0

--- app/chron_jobs.js
+++ app/chron_jobs.js
@@ -66,7 +66,6 @@ module.exports = {
                     if(count >= 2) {
                         this.timeRemaining--;
                     }
-                    this.timeRemaining--;
                 });
             } else {
                 this.timeRemaining--;
