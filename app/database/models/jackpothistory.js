75ab893e53f8e04ba63904cda78fec078fc238cc
Daniel Goje <gojedan98@yahoo.com>
2018-09-13 11:25:02 +0100

    Refactor jackpot and coinflip models and add procedures to a separate directory

--- app/database/models/jackpothistory.js
+++ app/database/models/jackpothistory.js
@@ -1,6 +1,4 @@
 'use strict';
-const request = require("request-promise"),
-      baseUri = "http://api.steampowered.com";
 
 module.exports = (sequelize, DataTypes) => {
   var JackpotHistory = sequelize.define('JackpotHistory', {
@@ -13,61 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     stakes: DataTypes.JSON
   }, {});
 
-  JackpotHistory.getHistory = function(query = {}, cb = null) {
-    JackpotHistory.findAll({
-      limit: 5,
-      where: query,
-      order: [[ "id", "DESC" ]]
-    }).then(history => {
-      let requestedUsers = {};
-      let userArray = [];
-      history.forEach(round => {
-        if(!requestedUsers[round.winner]) {
-          requestedUsers[round.winner] = {};
-          userArray.push(round.winner);
-        }
-        round.stakes = JSON.parse(round.stakes);
-        round.stakes.forEach(player => {
-          if(!requestedUsers[player.userId]) {
-            requestedUsers[player.userId] = {};
-            userArray.push(player.userId);
-          }
-          if(player.userId == round.winner) {
-            round.winner = {
-              id: round.winner,
-              total: player.total
-            }
-          }
-        });
-      });
-      request({
-        uri: `${baseUri}/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${userArray.join(',')}`,
-        json: true
-      }).then(body => {
-        body.response.players.forEach(profile => {
-          requestedUsers[profile.steamid] = {
-            name: profile.personaname,
-            avatar: profile.avatarfull
-          };
-        });
-        history.forEach(round => {
-          round.winner = {
-            ...round.winner,
-            ...requestedUsers[round.winner.id]
-          };
-          round.stakes.forEach((player) => {
-            player.userData = {
-              id: player.userId,
-              ...requestedUsers[player.userId]
-            };
-          });
-        });
-        if(cb) {
-          cb(history);
-        }
-      });
-    });
-  };
+  JackpotHistory.procedures = require("../procedures/jackpothistory");
 
   JackpotHistory.associate = function(models) {
     JackpotHistory.belongsTo(models.user, {foreignKey: "winner", targetKey: "steamId"});
---
+++ app/database/procedures/jackpothistory.js
@@ -0,0 +1,60 @@
+const request = require("request-promise"),
+      baseUri = "http://api.steampowered.com";
+
+module.exports = {
+    getHistory: function(query = {}, cb = null) {
+        JackpotHistory.findAll({
+            limit: 5,
+            where: query,
+            order: [[ "id", "DESC" ]]
+        }).then(history => {
+            let requestedUsers = {};
+            let userArray = [];
+            history.forEach(round => {
+            if(!requestedUsers[round.winner]) {
+                requestedUsers[round.winner] = {};
+                userArray.push(round.winner);
+            }
+            round.stakes = JSON.parse(round.stakes);
+            round.stakes.forEach(player => {
+                if(!requestedUsers[player.userId]) {
+                requestedUsers[player.userId] = {};
+                userArray.push(player.userId);
+                }
+                if(player.userId == round.winner) {
+                round.winner = {
+                    id: round.winner,
+                    total: player.total
+                }
+                }
+            });
+            });
+            request({
+            uri: `${baseUri}/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${userArray.join(',')}`,
+            json: true
+            }).then(body => {
+            body.response.players.forEach(profile => {
+                requestedUsers[profile.steamid] = {
+                name: profile.personaname,
+                avatar: profile.avatarfull
+                };
+            });
+            history.forEach(round => {
+                round.winner = {
+                ...round.winner,
+                ...requestedUsers[round.winner.id]
+                };
+                round.stakes.forEach((player) => {
+                player.userData = {
+                    id: player.userId,
+                    ...requestedUsers[player.userId]
+                };
+                });
+            });
+            if(cb) {
+                cb(history);
+            }
+            });
+        });
+        }
+};
